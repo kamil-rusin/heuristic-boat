06b67b54b206bcba37ae6a69a48af5e58a9e74e0
Kamil Rusin <lotrusek@gmail.com>
2020-01-02 14:46:27 +0100

    Wstępna wersja algorytmu + dodano suwak na granice w losowaniu nowego kątu.

--- public/math-service.js
+++ public/math-service.js
@@ -2,11 +2,11 @@
 //   współrzędną X
 //   współrzędną Y
 const evaluateBoatDesireVector = (initialAngle) => {
-  const {boatSpeed} = getParams();
-  return ({
-    x: -boatSpeed * Math.cos(initialAngle),
-    y: boatSpeed * Math.sin(initialAngle),
-  });
+    const {boatSpeed} = getParams();
+    return ({
+        x: -boatSpeed * Math.cos(initialAngle),
+        y: boatSpeed * Math.sin(initialAngle),
+    });
 };
 
 // wyznacza wynikowy wektor łodzi, uwzględniając prąd rzeki:
@@ -15,15 +15,16 @@ const evaluateBoatDesireVector = (initialAngle) => {
 //   kąt, pod jakim popłynie łódź
 //   długość wektora, czyli wynikową prędkość łódki
 const evaluateBoatResultVector = (initialAngle) => {
-  const {x, y} = evaluateBoatDesireVector(initialAngle);
-  const {riverSpeed} = getParams();
-  const resultX = -riverSpeed + x;
-  return ({
-    x: resultX,
-    y,
-    angle: Math.atan(-y / x),
-    length: Math.sqrt(x * x + y * y),
-  });
+    const {x, y} = evaluateBoatDesireVector(initialAngle);
+    const {riverSpeed} = getParams();
+    const resultX = -riverSpeed + x;
+    return ({
+        x: resultX,
+        y,
+        angle: Math.atan(-y / x),
+        length: Math.sqrt(x * x + y * y),
+    });
+    //TODO: współrzędna Y chyba źle
 };
 
 // wyznacza rezultat podróży łódki:
@@ -31,34 +32,78 @@ const evaluateBoatResultVector = (initialAngle) => {
 //   czas trwania podróży (docelowo w sekundach)
 //   odległość od punktu celu
 const evaluateBoatTripResults = (initialAngle) => {
-  const {angle, length} = evaluateBoatResultVector(initialAngle);
-  const betaAngle = Math.abs((Math.PI / 2) - angle);
-  const {destinationLocation, riverWidth} = getParams();
+    const {angle, length} = evaluateBoatResultVector(initialAngle);
+    const betaAngle = Math.abs((Math.PI / 2) - angle);
+    const {destinationLocation, riverWidth} = getParams();
 
-  let resultLocation = riverWidth * Math.tan(betaAngle);
-  if (angle < Math.PI / 2) {
-    resultLocation = -resultLocation;
-  }
+    let resultLocation = riverWidth * Math.tan(betaAngle);
+    if (angle < Math.PI / 2) {
+        resultLocation = -resultLocation;
+    }
 
-  const tripLength = riverWidth / Math.cos(betaAngle);
+    const tripLength = riverWidth / Math.cos(betaAngle);
 
-  return ({
-    location: resultLocation,
-    duration: Math.abs(tripLength / length),
-    distance: Math.abs(resultLocation - destinationLocation),
-  });
+    return ({
+        location: resultLocation,
+        duration: Math.abs(tripLength / length),
+        distance: Math.abs(resultLocation - destinationLocation),
+    });
 };
 
 // funkcja celu w uproszczonej postaci
 const objectiveFunction = (initialAngle) => {
-  return evaluateBoatTripResults(initialAngle).distance;
+    return evaluateBoatTripResults(initialAngle).distance;
+};
+
+//metoda na losowanie z przedziału(min= -factor, max = factor)
+const generateRandomNumber = (factor) => {
+    return (Math.random() * (Number(factor) + Number(factor)) - Number(factor));
 };
 
 // metoda algorytmu heurystycznego
 const evaluateOptimalBoatAngle = () => {
-  let m, n, o, p, q; // TODO: dodać suwaki na parametry algorytmu
+    const {iterations, agents, boatInitialAngleInRadians, neighborhoodFactor} = getParams();
+    let bestDistance, historyAngles = [], historyDistance = [], iter = 0, tempAngle, tempAgent,
+        tempDistance, forValue,
+        tempValue;
 
-  // TODO: algorytm heurystyczny
+    let bestAngle = boatInitialAngleInRadians;
 
-  return Math.random() * 180;
+    while (iter < iterations) {
+        bestDistance = objectiveFunction(bestAngle);
+
+        for (let i = 0; i < agents; i++) {
+            tempAgent = Number(bestAngle) + Number(generateRandomNumber(neighborhoodFactor));
+
+            //granice: 0 - 180 stopni
+            if (tempAgent > 3.14159265) {
+                tempAgent = 3.14159265;
+            } else if (tempAgent < 0) {
+                tempAgent = 0;
+            }
+
+            if (i === 0) {
+                forValue = objectiveFunction(tempAgent);
+            } else {
+                tempValue = objectiveFunction(tempAgent);
+                if (tempValue < forValue) {
+                    tempDistance = tempValue;
+                    tempAngle = tempAgent;
+                }
+            }
+        }
+
+        if (tempDistance < bestDistance) {
+            bestAngle = tempAngle;
+        }
+
+        historyAngles.push(bestAngle);
+        historyDistance.push(bestDistance);
+        iter++;
+    }
+
+    return ({
+        historyDistance: historyDistance,
+        historyAngles: historyAngles,
+    });
 };
