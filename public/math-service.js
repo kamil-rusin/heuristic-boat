2d5e5a386af63e1feec42261c7338ed3746444e1
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2020-01-03 21:14:21 +0100

    Cofnięcie refaktorycacji wcięć w istniejącym kodzie

--- public/math-service.js
+++ public/math-service.js
@@ -2,11 +2,11 @@
 //   współrzędną X
 //   współrzędną Y
 const evaluateBoatDesireVector = (initialAngle) => {
-    const {boatSpeed} = getParams();
-    return ({
-        x: -boatSpeed * Math.cos(initialAngle),
-        y: boatSpeed * Math.sin(initialAngle),
-    });
+  const {boatSpeed} = getParams();
+  return ({
+    x: -boatSpeed * Math.cos(initialAngle),
+    y: boatSpeed * Math.sin(initialAngle),
+  });
 };
 
 // wyznacza wynikowy wektor łodzi, uwzględniając prąd rzeki:
@@ -15,15 +15,15 @@ const evaluateBoatDesireVector = (initialAngle) => {
 //   kąt, pod jakim popłynie łódź
 //   długość wektora, czyli wynikową prędkość łódki
 const evaluateBoatResultVector = (initialAngle) => {
-    const {x, y} = evaluateBoatDesireVector(initialAngle);
-    const {riverSpeed} = getParams();
-    const resultX = -riverSpeed + x;
-    return ({
-        x: resultX,
-        y,
-        angle: Math.atan(-y / x),
-        length: Math.sqrt(x * x + y * y),
-    });
+  const {x, y} = evaluateBoatDesireVector(initialAngle);
+  const {riverSpeed} = getParams();
+  const resultX = -riverSpeed + x;
+  return ({
+    x: resultX,
+    y,
+    angle: Math.atan(-y / x),
+    length: Math.sqrt(x * x + y * y),
+  });
 };
 
 // wyznacza rezultat podróży łódki:
@@ -31,27 +31,27 @@ const evaluateBoatResultVector = (initialAngle) => {
 //   czas trwania podróży (docelowo w sekundach)
 //   odległość od punktu celu
 const evaluateBoatTripResults = (initialAngle) => {
-    const {angle, length} = evaluateBoatResultVector(initialAngle);
-    const betaAngle = Math.abs((Math.PI / 2) - angle);
-    const {destinationLocation, riverWidth} = getParams();
+  const {angle, length} = evaluateBoatResultVector(initialAngle);
+  const betaAngle = Math.abs((Math.PI / 2) - angle);
+  const {destinationLocation, riverWidth} = getParams();
 
-    let resultLocation = riverWidth * Math.tan(betaAngle);
-    if (angle < Math.PI / 2) {
-        resultLocation = -resultLocation;
-    }
+  let resultLocation = riverWidth * Math.tan(betaAngle);
+  if (angle < Math.PI / 2) {
+    resultLocation = -resultLocation;
+  }
 
-    const tripLength = riverWidth / Math.cos(betaAngle);
+  const tripLength = riverWidth / Math.cos(betaAngle);
 
-    return ({
-        location: resultLocation,
-        duration: Math.abs(tripLength / length),
-        distance: Math.abs(resultLocation - destinationLocation),
-    });
+  return ({
+    location: resultLocation,
+    duration: Math.abs(tripLength / length),
+    distance: Math.abs(resultLocation - destinationLocation),
+  });
 };
 
 // funkcja celu w uproszczonej postaci
 const objectiveFunction = (initialAngle) => {
-    return evaluateBoatTripResults(initialAngle).distance;
+  return evaluateBoatTripResults(initialAngle).distance;
 };
 
 //metoda na losowanie z przedziału(min= -factor, max = factor)
